e36533e89a5df4a7fa6bf246359b76dea50350e9
Dell <Dell@DESKTOP-QIAOHDH>
2018-05-12 20:10:32 +0200

    test

---
+++ Maventest/src/test/java/maventst.java
@@ -0,0 +1,14 @@
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class maventst {
+
+	@Test
+	public void TC_01()
+	{
+		Assert.assertTrue(true);
+	}	
+}
